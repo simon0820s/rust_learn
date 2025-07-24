ce77d264c2921797308149793024c9d1fac214ed
simon0820s <simon0820s@gmail.com>
2025-07-24 18:02:00 -0500

    feat(mean_squared_error): get mean_squared error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+
+    let train_data: Vec<(i32, i32)> = vec![
+        (0, 4),
+        (1, 7),
+        (2, 10),
+        (3, 13),
+        (4, 16),
+        (5, 19),
+        (6, 22),
+        (7, 25),
+        (8, 28),
+        (9, 31),
+    ];
+
+    let b = 1;
+    let m = 1;
+    let mut total_error = 0;
+
+    // Get error by point 
+    train_data.iter().for_each(|(x, y)| {
+        println!("x: {}, y: {}", x, y);
+        total_error += (y - (m * x + b)).pow(2).abs();
+    });
+
+    // Calculate mean squared error
+    let mean_squared_error = total_error as f64 / train_data.len() as f64;
+
 }
