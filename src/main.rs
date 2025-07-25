fab2a1d75a4fa08d3b0c24824dc20df59965971e
Santiago Uribe <sauribee@unal.edu.co>
2025-07-24 21:05:54 -0500

    24/07/2025

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
 fn main() {
+    let iterations: i32 = 10000; // Number of iterations for training
+    let learning_rate: f64 = 0.0125; // Learning rate for gradient descent
 
-    let train_data: Vec<(i32, i32)> = vec![
-        (0, 4),
-        (1, 7),
-        (2, 10),
-        (3, 13),
-        (4, 16),
-        (5, 19),
-        (6, 22),
-        (7, 25),
-        (8, 28),
-        (9, 31),
+    let train_data: Vec<(f64, f64)> = vec![
+        (0.0, 4.0),
+        (1.0, 7.0),
+        (2.0, 10.0),
+        (3.0, 13.0),
+        (4.0, 16.0),
+        (5.0, 19.0),
+        (6.0, 22.0),
+        (7.0, 25.0),
+        (8.0, 28.0),
+        (9.0, 31.0),
+        (10.0, 34.0),
     ];
 
-    let b = 1;
-    let m = 1;
-    let mut total_error = 0;
+    let train_data_len = train_data.len();
+    let mut w = 1.0; // Weight
+    let mut b = 1.0; // Bias
+    
+    for _ in 0..iterations {
 
-    // Get error by point 
-    train_data.iter().for_each(|(x, y)| {
-        println!("x: {}, y: {}", x, y);
-        total_error += (y - (m * x + b)).pow(2).abs();
-    });
+        w -= learning_rate * train_data.iter().fold(0.0, |acc, &(x, y)| {
+            acc + 2.0 * x * ((w * x + b) - y)
+        }) / train_data_len as f64;
 
-    // Calculate mean squared error
-    let mean_squared_error = total_error as f64 / train_data.len() as f64;
+        b -= learning_rate * train_data.iter().fold(0.0, |acc, &(x, y)| {
+            acc + 2.0 * ((w * x + b) - y)
+        }) / train_data_len as f64;
+    }
 
+    println!("Final weight: {}", w);
+    println!("Final bias: {}", b);
 }
